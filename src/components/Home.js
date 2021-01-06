f68224959effe43ffd32f8042e34ca45c245ebed
Sufyan Raza <34751328+Sufyanraza50@users.noreply.github.com>
2021-01-06 22:10:56 +0500

    product section complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './Home.css';
+import Product from './Product';
 
 export const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home">
+            <img className="home__image" src="https://i.ibb.co/RPpKwv6/newbannerhelp-1200-amazon-glacier-final-01-01.png" alt="banner-image" />
+            <div className="home__row">
+                <Product 
+                    id="121314"
+                    title="The Lean Startup: How contant innovation"
+                    price={11.96}
+                    rating={4}
+                    image="https://i.ibb.co/M2PQy0m/81jg-Ci-NJPUL.jpg" 
+                />
+                <Product 
+                    id="121314"
+                    title="The Lean Startup: How contant innovation"
+                    price={15.44}
+                    rating={5}
+                    image="https://i.ibb.co/M2PQy0m/81jg-Ci-NJPUL.jpg" 
+                />
+                <Product 
+                    id="121314"
+                    title="The Lean Startup: How contant innovation"
+                    price={9.99}
+                    rating={3}
+                    image="https://i.ibb.co/M2PQy0m/81jg-Ci-NJPUL.jpg" 
+                />
+            </div>
         </div>
     )
 }
