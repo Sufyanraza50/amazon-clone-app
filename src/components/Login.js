72b6661409356a96253f43c7ffcce3446ea0b55e
Sufyan Raza <34751328+Sufyanraza50@users.noreply.github.com>
2021-01-06 19:24:58 +0500

    Login page desing complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
     return (
         <div className="login">
+            <Link>
+                <img src="https://i.ibb.co/SBG91xy/amazon-logo-transparent.png" alt="home-logo" className="login__logo" />
+            </Link>
             <div className="login__container">
-                <h1>Log in</h1>
+                <h1>Sign in</h1>
                 <form>
                     <h5>Email:</h5>
-                    <input type="email"/>
+                    <input type="email" />
                     <h5>Password</h5>
                     <input type="password" />
                     <button type="submit" className="login__signInButton">Sign In</button>
-                    <p>Ny signing- you are agreeing to term's and conditions</p>
+                    <p>By Signing In - You are agreeing to Term's and Conditions</p>
+                    <button className="login__registerButton">Create Account</button>
                 </form>
             </div>
         </div>
