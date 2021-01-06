21c7eb2b7f0fa1e518dd88d81435d8cc48a1c4fd
Sufyan Raza <34751328+Sufyanraza50@users.noreply.github.com>
2021-01-06 18:22:28 +0500

    Login form working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="login">
+            <div className="login__container">
+                <h1>Log in</h1>
+                <form>
+                    <h5>Email:</h5>
+                    <input type="email"/>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
