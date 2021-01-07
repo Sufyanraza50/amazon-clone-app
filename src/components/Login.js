a93799805c00b40ced4dc3558bd17dae70e6f644
Sufyan Raza <34751328+Sufyanraza50@users.noreply.github.com>
2021-01-07 21:02:01 +0500

    Sign In and Create Account code complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from './firbase';
 import './Login.css'
 
 const Login = () => {
+    const history = useHistory()
+    const [useremail, setUserEmail] = useState();
+    const [userpassword, setUserPassword] = useState();
+
+    const loginuser = event => {
+        event.preventDefault()
+        auth.signInWithEmailAndPassword(useremail, userpassword)
+        .then((auth) => {
+            history.push('/')
+        })
+        .catch(e => alert(e.message))
+    }
+
+    const signupuser = event => {
+        event.preventDefault()
+        auth.createUserWithEmailAndPassword(useremail, userpassword)
+        .then(auth => {
+            history.push('/')
+        })
+        .catch(e => alert(e.message))
+
+    }
+
     return (
         <div className="login">
             <Link>
@@ -12,12 +36,12 @@ const Login = () => {
                 <h1>Sign in</h1>
                 <form>
                     <h5>Email:</h5>
-                    <input type="email" />
+                    <input type="email" value={useremail} onChange={event => setUserEmail(event.target.value)} />
                     <h5>Password</h5>
-                    <input type="password" />
-                    <button type="submit" className="login__signInButton">Sign In</button>
+                    <input type="password" value={userpassword} onChange={event => setUserPassword(event.target.value)} />
+                    <button onClick={loginuser} type="submit" className="login__signInButton">Sign In</button>
                     <p>By Signing In - You are agreeing to Term's and Conditions</p>
-                    <button className="login__registerButton">Create Account</button>
+                    <button onClick={signupuser} className="login__registerButton">Create Account</button>
                 </form>
             </div>
         </div>
